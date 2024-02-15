6affe22dc0e094d90ec6f68317735bb899d28fd8
Kallissalaz <143545196+Kallissalaz@users.noreply.github.com>
2024-02-15 12:51:37 -0500

    Add files via upload

--- Python HW1/PE2_2.py
+++ Python HW1/PE2_2.py
@@ -1,5 +1,11 @@
 intrest= 0.05 #interest is suppose to come better the number
 balance=10
-print('intrest*balance')
-
-bal=123
+print(intrest*balance)
+balance=123
+deposit=100
+print(balance+deposit)
+a=2
+b=3
+c=a+b
+print(c)
+# this whole equation is incorrect because you can't add a string to a number.
