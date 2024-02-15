a18487169367e838580af5e73ff042d7974afc9b
Kallissalaz <143545196+Kallissalaz@users.noreply.github.com>
2024-02-15 09:35:44 -0500

    Add files via upload

--- Python HW1/PE2_4.py
+++ Python HW1/PE2_4.py
@@ -1,2 +1,10 @@
-str1="kallissalaz@gmail.com"
-print()
+#Create variable email and store your email infoprmation to the variable.
+email="kallissal@nycstudents.net" #similar mjordannba.com
+#b) Use slicing to print the email address.
+print(email[:])
+#c)Use slicing and string methods to p[rint only user name all in lowercase
+username=email[0:9]
+print(username.lower())
+#d)Use slicing and string methods to print only the company name all in sequence
+companyName=(email[10:20])
+print(companyName.upper())
